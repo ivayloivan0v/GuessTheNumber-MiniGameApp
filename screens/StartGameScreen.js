79b4f09eb94aeae6599a05a276e6f6471f63620a
Ivaylo <ivayloivan0v@icloud.com>
2022-09-03 17:31:46 +0300

    creating custom buttons

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { TextInput, View } from "react-native";
+
+import { PrimaryButton } from "../components/PrimaryButton";
+
+export const StartGameScreen = () => {
+  return (
+    <View>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+};
